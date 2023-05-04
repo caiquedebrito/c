976e831f94c3f558b3497870b6bfd81d65bbece9
Caique De Brito <caiquedebritoo@gmail.com>
2023-05-03 21:27:49 -0300

    adicionado exemplo de uso de pilha
    - conversão decimal-binário com o uso de pilha

---
+++ estrutura-de-dados/pilha/exemplos/binario.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "../pilha.h"
+
+int main(void) {
+  int n;
+  Pilha P = pilha(32);
+
+  printf("Decimal? ");
+  scanf("%d", &n);
+
+  do { empilha(n%2, P); n /= 2; } while(n != 0);
+  
+  printf("Binario: ");
+  while (!vaziap(P)) { printf("%d ", desempliha(P)); }
+
+  return 0;
+}
