0871ec6fffe13537ff8d1c65449229a79b6427c4
caiquedebrito <88737351+caiquedebrito@users.noreply.github.com>
2023-05-30 00:24:06 +0000

    adicionado implementação da lista encadeada

--- estrutura-de-dados/lista/lista_encadeada.c
+++ estrutura-de-dados/lista/lista_encadeada.c
@@ -1,117 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct reg {
-    int conteudo;
-    struct reg *prox;
+typedef struct NODO{
+    int     conteudo;
+    struct  NODO *prox;
 } celula;
 
-celula *inserir(int x, celula *p) {
-    // Criação de um novo elemento
-    celula *nova;
-    nova = malloc(sizeof(celula));
-    nova->conteudo = x;
-    nova->prox = NULL;
-    
-    // Inseri nova no inicioda lista
-    if (p != NULL) nova->prox = p;
-    p = nova;
-    
-    return p;
-}
-
-void imprimir(celula *lista) {
-    if (lista != NULL) {
-        printf("%d\n", lista->conteudo);
-        imprimir(lista->prox);
-    }
-}
-
-celula *inserir_final(int x, celula *p) {
-    celula *novo, *topo = p;
-    
-    novo = malloc(sizeof(celula));
+celula *inserirNoFinal(int x, celula *p){
+    celula *novo, *topo;
+    topo = p;
+    novo = malloc (sizeof (celula));
     novo->conteudo = x;
     novo->prox = NULL;
-    
-    if (p != NULL) {
-       // Acessar o último elemento
-       for (p; p->prox != NULL; p = p->prox);
-       p->prox = novo;
-    } else topo = novo;
-    
+    if (p!=NULL){
+        for(p; p->prox != NULL;p=p->prox);
+        p->prox=novo;
+    }else topo = novo;
     return topo;
 }
 
-celula *buscar(int x, celula *lista) {
-    celula *p;
-    p = lista;
-    
-    while(p != NULL && p->conteudo != x)
-        p = p->prox;
-        
-    return p;
+celula *buscar(int x, celula *lista){
+    celula *aux = lista;
+    while(aux!=NULL && aux->conteudo!=x)
+       aux=aux->prox;
+    return aux;  
 }
 
-celula *buscar_recursivo(int x, celula *lista) {
+celula *buscarRecursivo(int x, celula *lista){
     if (lista == NULL) return NULL;
-    
-    if (lista->conteudo == x) return lista;
-    
-    return buscar_recursivo(x, lista->prox);
+    if (lista->conteudo!=x) return buscarRecursivo(x,lista->prox);
+    else return lista;
 }
 
-celula *buscar_remover(int x, celula *lista) {
-    celula *p, *q;
-    
-    p = lista;
-    
-    if (p->conteudo == x) {
-        q = lista;
-        p = lista->prox;
-        free(q);
-        return p;
-    }
-    
-    q = lista->prox;
-    
-    while (q != NULL && q->conteudo != x) {
-        p = q;
-        q = q->prox;
-    }
-    
-    if (q != NULL) {
-        p->prox = q->prox;
-        free(q);
-    }
-    
-    return lista;
+void remover(celula *p){
+   celula *lixo;
+   lixo = p->prox;
+   p->prox = lixo->prox;
+   free (lixo);
 }
 
-int main()
-{
-    int resp, resp2 = 1;
-    printf("Lista Simplesmente Encadeada\n");
+celula *buscar_e_remover(int x, celula *listaEncadeada){
+   celula *p, *q;
+   p = listaEncadeada;
+   if(p->conteudo==x){
+       q=listaEncadeada;
+       p=listaEncadeada->prox;
+       free(q);
+       return p;
+   }else{
+       q = listaEncadeada->prox;
+       while (q != NULL && q->conteudo != x) {
+            p = q;
+            q = q->prox;
+        }
+        if (q != NULL) {
+            p->prox = q->prox;
+            free (q);
+        }
+        return listaEncadeada;
+   }    
+}
+
+
+celula *inserirNoInicio(int x, celula *p){
+   celula *nova;
+    nova = malloc (sizeof (celula));
+    nova->conteudo = x;
+    nova->prox = NULL;
+   if (p != NULL)
+        nova->prox = p;
+   p = nova;
+   return p;
+}
+
+void imprimir(celula *listaEncadeada) {
+   celula *p;
+   for (p = listaEncadeada; p != NULL; p = p->prox)
+      printf ("%d\n", p->conteudo);
+}
+
+int main(){
+    int resp, resp2=1;
+    printf("Lista Simplesmente Encadeada \n");
     celula *topo, *buscado;
-    
-    for (int i = 0; i < 10; i++) {
+   
+    for(int j=0; j<10;j++){
         printf("Digite o próximo elemento da lista: ");
         scanf("%d", &resp);
-        topo = inserir_final(resp, topo);
-    }
-    printf("Fim da insercao\n");
-    
-    while (resp2) {
-        printf("Digite o elemento que deseja remover da lista: ");
-        scanf("%d", &resp);
-        
-        topo = buscar_remover(resp, topo);
-        
+        topo = inserirNoFinal(resp, topo);
         imprimir(topo);
-        
-        printf("Deseja repetir a busca? 1 - Sim, 0 - Nao\n");
-        scanf("%d", &resp2);
     }
-
-    return 0;
-}
+    printf("Fim da Inserção \n");
+   
+    while (resp2){
+        printf("Digite o elemento que deseja encontrar na lista: ");
+        scanf("%d", &resp);
+   
+        topo = buscar_e_remover(resp, topo);
+       
+        imprimir(topo);
+       
+       printf("Deseja repetir a busca? 1 - Sim, 0 - Não\n");
+       scanf("%d", &resp2);
+    }
